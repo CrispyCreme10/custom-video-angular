ab00ed9262f75bdf4e4a28d660b17e10e514a683
CrispyCreme10 <jordanrenner69@gmail.com>
2023-08-31 18:40:55 -0500

    oh yeah

---
+++ src/app/components/video/video.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { WebVTTCue, parseWebVTT } from 'src/resources/webvtt-parser/parser';
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.scss']
+})
+export class VideoComponent implements OnInit, AfterViewInit {
+  @ViewChild('track') trackElem!: ElementRef<HTMLTrackElement>;
+
+  transcriptUrl = 'assets/test.vtt';
+
+  cues: WebVTTCue[] = [];
+
+  constructor(
+    private renderer: Renderer2,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.extractTranscript();
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.trackElem.nativeElement.track);
+    this.renderer.listen(this.trackElem.nativeElement, 'cuechange', (event: Event) => {
+      console.log('cuechange: ', event);
+      console.log((event.target as HTMLTrackElement).track);
+    });
+  }
+
+  private extractTranscript() {
+    this.http.get(this.transcriptUrl, {
+      headers: new HttpHeaders().set('Content-Type', 'text/vtt'),
+      responseType: 'text'
+    }).subscribe(data => {
+      // console.log(data);
+      this.cues = parseWebVTT(data);
+      // console.log(this.cues);
+    })
+  }
+}
---
+++ src/resources/webvtt-parser/parser.ts
@@ -0,0 +1,67 @@
+export interface WebVTTCue {
+  startTime: number;
+  endTime: number;
+  text: string[];
+}
+
+export function parseWebVTT(text: string): WebVTTCue[] {
+  text = text.replace(/(\r\n|\n|\r)/g, '\n');
+  text = text.replace('(', '- ');
+  text = text.replace(')', ' -');
+  
+  // tokenize
+  let tokens = text.split('\n');
+
+  // shave off some fat
+  const exlusions = ['WEBVTT', '']
+  tokens = tokens.filter(t => !exlusions.includes(t));
+
+  // create cues
+  const cues: WebVTTCue[] = [];
+  let currentCue: WebVTTCue | undefined = undefined;
+  let consumingCueText = false;
+  for(const token of tokens) {
+    if (token.includes('-->')) {
+      if (currentCue !== undefined) {
+        cues.push(currentCue);
+      }
+
+      const split = token.split('-->');
+      split.forEach(s => s.trim());
+      currentCue = {
+        startTime: convertTimeString(split[0]),
+        endTime: convertTimeString(split[1]),
+        text: []
+      };
+      consumingCueText = true;
+      continue;
+    } else {
+      const val = token.replace('- ', '');
+      currentCue?.text.push(val);
+    }
+  }
+
+  if (currentCue !== undefined) {
+    cues.push(currentCue);
+  }
+
+  return cues;
+}
+
+function convertTimeString(timeStr: string): number {
+  const split = timeStr.split(':');
+  if (split?.length === 3) {
+    const hours = Number(split[0]);
+    const min = Number(split[1]);
+    const sec = Number(split[2]);
+
+    return hours * 3600 + min * 60 + sec;
+  } else if (split?.length === 2) {
+    const min = Number(split[0]);
+    const sec = Number(split[1]);
+
+    return min * 60 + sec;
+  }
+
+  return 0;
+}
